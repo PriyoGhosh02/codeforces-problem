0a0183ac901fa0b9c661b31ecc55e0c8461e6973
priyo ghosh <priyoghosh02@gmail.com>
2025-03-04 23:13:05 +0600

    create laptop brance

--- Vanya_and_Fence.py
+++ Vanya_and_Fence.py
@@ -1,11 +1,11 @@
-from math import ceil
-n,h=map(int, input().split())
-a=list(map(int, input().split()))
+# from math import ceil
+# n,h=map(int, input().split())
+# a=list(map(int, input().split()))
 
-if n==len(a):
-    width=0
-    for i in range(n):
-        temp=ceil(a[i]/h)
-        width+=temp
+# if n==len(a):
+#     width=0
+#     for i in range(n):
+#         temp=ceil(a[i]/h)
+#         width+=temp
 
-print(width)
+# print(width)
