9720c10d181a8be6cd6bd3bc630c3bfbedc8d7ff
PriyoGhosh02 <priyoghosh02@gmail.com>
2025-04-27 17:15:43 +0600

    solved Candies and Two Sisters

--- 1spaceFill_.py
+++ 1spaceFill_.py
@@ -1,4 +1,11 @@
-s=input()
+s = input()
 split = s.split()
 
-print('_'.join(split))
+print("_".join(split))
+
+# git add .
+# git commit -m "commit"
+# git push -u origin desktop
+
+# create a new branch
+# git checkout -b new_branch_name
