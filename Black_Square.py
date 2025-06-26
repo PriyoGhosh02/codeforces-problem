5bcc5403fb7aa2e498bae677a6271581710e22ba
PriyoGhosh02 <priyoghosh02@gmail.com>
2025-06-27 01:19:27 +0600

    solved Black_Square

---
+++ Black_Square.py
@@ -0,0 +1,15 @@
+a,b,c,d=map(int,input().split())
+s=input()
+cal=0
+
+for i in s:
+    if i == '1':
+        cal += a
+    elif i == '2':
+        cal += b
+    elif i == '3':
+        cal += c
+    elif i == '4':
+        cal += d
+        
+print(cal)
